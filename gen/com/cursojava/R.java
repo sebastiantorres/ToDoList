7020aae7ed4fdbb36ef852ad1b9d16492613f3b1
sebastian torres <sebag.torres@gmail.com>
2012-04-09 19:30:45 -0300

    actualizacion nkp

--- gen/com/cursojava/R.java
+++ gen/com/cursojava/R.java
@@ -13,6 +13,10 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f060001;
+        public static final int listcolor=0x7f060000;
+    }
     public static final class drawable {
         public static final int accept=0x7f020000;
         public static final int add=0x7f020001;
@@ -22,19 +26,38 @@ public final class R {
         public static final int zoom=0x7f020005;
     }
     public static final class id {
-        public static final int add=0x7f060008;
-        public static final int btnAceptar=0x7f060006;
-        public static final int btnCancelar=0x7f060005;
-        public static final int dpFechaFin=0x7f060004;
-        public static final int dpFechaInicio=0x7f060003;
-        public static final int etdescripcion=0x7f060001;
-        public static final int ettitulo=0x7f060000;
-        public static final int ivAddTarea=0x7f060007;
-        public static final int spprioridad=0x7f060002;
+        public static final int btnAceptar=0x7f070010;
+        public static final int btnCancelar=0x7f070011;
+        public static final int btnListo=0x7f070017;
+        public static final int delete=0x7f070014;
+        public static final int dpFechaFin=0x7f07000e;
+        public static final int dpFechaInicio=0x7f07000b;
+        public static final int etdescripcion=0x7f070005;
+        public static final int ettitulo=0x7f070002;
+        public static final int icon=0x7f070018;
+        public static final int ivAddTarea=0x7f070013;
+        public static final int label=0x7f070019;
+        public static final int lblTitulo=0x7f070001;
+        public static final int lbldescripcion=0x7f070004;
+        public static final int lblfin=0x7f07000d;
+        public static final int lblinicio=0x7f07000a;
+        public static final int lblprioridad=0x7f070007;
+        public static final int linearLayout1=0x7f070012;
+        public static final int linearLayout2=0x7f070000;
+        public static final int linearLayout3=0x7f070003;
+        public static final int linearLayout4=0x7f070006;
+        public static final int linearLayout5=0x7f070009;
+        public static final int linearLayout6=0x7f07000c;
+        public static final int linearLayout7=0x7f07000f;
+        public static final int resultado=0x7f070016;
+        public static final int spprioridad=0x7f070008;
+        public static final int textView1=0x7f070015;
     }
     public static final class layout {
         public static final int addtarea=0x7f030000;
         public static final int list=0x7f030001;
+        public static final int resulttarea=0x7f030002;
+        public static final int row=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
